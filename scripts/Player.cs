0459d5ec716f957360f86031e71a76ad8fb3dc3e
Levi Dalrymple <levidalrymple1221@gmail.com>
2025-07-03 18:20:41 -0500

    Init Commit

--- scripts/Player.cs
+++ scripts/Player.cs
@@ -18,48 +18,18 @@ public partial class Player : TurnMovement
 		{
 			WaitTime = _moveSpeed,
 			OneShot = true,
-			// Snaps the mouse position to the grid and moves the player there
-	public void GetMousePositionOnGridAndMove()
-	{
-		var globalMouse = GetGlobalMousePosition();
-		var localMouse = globalMouse + GetParent<Node2D>().Position;
-		int x = Mathf.RoundToInt(localMouse.X / GridSize);
-		int y = Mathf.RoundToInt(localMouse.Y / GridSize);
-		var snappedPosition = new Vector2(x * GridSize, y * GridSize);
-		Position = snappedPosition;
-	}
-};
+		};
 		this.AddChild(_timer);
 
 		base._Ready();
-		// Snaps the mouse position to the grid and moves the player there
-	public void GetMousePositionOnGridAndMove()
-	{
-		var globalMouse = GetGlobalMousePosition();
-		var localMouse = globalMouse + GetParent<Node2D>().Position;
-		int x = Mathf.RoundToInt(localMouse.X / GridSize);
-		int y = Mathf.RoundToInt(localMouse.Y / GridSize);
-		var snappedPosition = new Vector2(x * GridSize, y * GridSize);
-		Position = snappedPosition;
 	}
-}
 
 	public override void _Process(double dt)
 	{
 		base._Process(dt);
 		if (Engine.IsEditorHint()) return;
 		Move();
-		// Snaps the mouse position to the grid and moves the player there
-	public void GetMousePositionOnGridAndMove()
-	{
-		var globalMouse = GetGlobalMousePosition();
-		var localMouse = globalMouse + GetParent<Node2D>().Position;
-		int x = Mathf.RoundToInt(localMouse.X / GridSize);
-		int y = Mathf.RoundToInt(localMouse.Y / GridSize);
-		var snappedPosition = new Vector2(x * GridSize, y * GridSize);
-		Position = snappedPosition;
 	}
-}
 
 
 	private void Move()
@@ -72,84 +42,24 @@ public partial class Player : TurnMovement
 		if (Input.IsActionPressed("up"))
 		{
 			movement.Y -= _moveDistance;
-			// Snaps the mouse position to the grid and moves the player there
-	public void GetMousePositionOnGridAndMove()
-	{
-		var globalMouse = GetGlobalMousePosition();
-		var localMouse = globalMouse + GetParent<Node2D>().Position;
-		int x = Mathf.RoundToInt(localMouse.X / GridSize);
-		int y = Mathf.RoundToInt(localMouse.Y / GridSize);
-		var snappedPosition = new Vector2(x * GridSize, y * GridSize);
-		Position = snappedPosition;
-	}
-} 
+		} 
 		
 		if (Input.IsActionPressed("down"))
 		{
 			movement.Y  += _moveDistance;
-			// Snaps the mouse position to the grid and moves the player there
-	public void GetMousePositionOnGridAndMove()
-	{
-		var globalMouse = GetGlobalMousePosition();
-		var localMouse = globalMouse + GetParent<Node2D>().Position;
-		int x = Mathf.RoundToInt(localMouse.X / GridSize);
-		int y = Mathf.RoundToInt(localMouse.Y / GridSize);
-		var snappedPosition = new Vector2(x * GridSize, y * GridSize);
-		Position = snappedPosition;
-	}
-}
+		}
 
 		if (Input.IsActionPressed("left"))
 		{
 			movement.X -= _moveDistance;
-			// Snaps the mouse position to the grid and moves the player there
-	public void GetMousePositionOnGridAndMove()
-	{
-		var globalMouse = GetGlobalMousePosition();
-		var localMouse = globalMouse + GetParent<Node2D>().Position;
-		int x = Mathf.RoundToInt(localMouse.X / GridSize);
-		int y = Mathf.RoundToInt(localMouse.Y / GridSize);
-		var snappedPosition = new Vector2(x * GridSize, y * GridSize);
-		Position = snappedPosition;
-	}
-}
+		}
 
 		if (Input.IsActionPressed("right"))
 		{
 			movement.X += _moveDistance;
-			// Snaps the mouse position to the grid and moves the player there
-	public void GetMousePositionOnGridAndMove()
-	{
-		var globalMouse = GetGlobalMousePosition();
-		var localMouse = globalMouse + GetParent<Node2D>().Position;
-		int x = Mathf.RoundToInt(localMouse.X / GridSize);
-		int y = Mathf.RoundToInt(localMouse.Y / GridSize);
-		var snappedPosition = new Vector2(x * GridSize, y * GridSize);
-		Position = snappedPosition;
-	}
-}
+		}
 
 		var actualMovement = movement * GridSize;
 		Position += actualMovement;
-		// Snaps the mouse position to the grid and moves the player there
-	public void GetMousePositionOnGridAndMove()
-	{
-		var globalMouse = GetGlobalMousePosition();
-		var localMouse = globalMouse + GetParent<Node2D>().Position;
-		int x = Mathf.RoundToInt(localMouse.X / GridSize);
-		int y = Mathf.RoundToInt(localMouse.Y / GridSize);
-		var snappedPosition = new Vector2(x * GridSize, y * GridSize);
-		Position = snappedPosition;
-	}
-}
-	// Snaps the mouse position to the grid and moves the player there
-	public void GetMousePositionOnGridAndMove()
-	{
-		var globalMouse = GetGlobalMousePosition();
-		var localMouse = globalMouse + GetParent<Node2D>().Position;
-		int x = Mathf.RoundToInt(localMouse.X / GridSize);
-		int y = Mathf.RoundToInt(localMouse.Y / GridSize);
-		var snappedPosition = new Vector2(x * GridSize, y * GridSize);
-		Position = snappedPosition;
 	}
 }
